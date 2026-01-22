e28fecfc4612be79e004caf59e82d6a1a5fc5258
briannaordaz <briannaordaz5@gmail.com>
2026-01-21 20:10:28 -0800

    entered first number

---
+++ COMP003AL01.LectureActivity2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string userInput = Console.ReadLine();
+        int UserInput;
+        UserInput = 5;
+        Console.ReadLine();
+
+        
+
+    }
+}
+
+
