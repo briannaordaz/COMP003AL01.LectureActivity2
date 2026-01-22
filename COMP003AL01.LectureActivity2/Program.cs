2590719a294f5a4b3efd25fbed2efb3ebb05ef14
briannaordaz <briannaordaz5@gmail.com>
2026-01-21 22:42:19 -0800

    completed steps 5 and 6

--- COMP003AL01.LectureActivity2/Program.cs
+++ COMP003AL01.LectureActivity2/Program.cs
@@ -7,18 +7,27 @@ class Program
     static void Main(string[] args)
     {
         
+        //Step 3: Prompt and Read Two Inputs & Step 4: Parse The Inputs into double Values
         Console.WriteLine("Enter The First Number:");
         double userInput = double.Parse(Console.ReadLine());
         
         
-        
         Console.WriteLine("Enter The Second Number:");
         double userInput2 = double.Parse(Console.ReadLine());
-    
+
         
+        //Step 5: Compute Required Results:
+        double sumvalue = (userInput + userInput2);
+        double averagevalue = sumvalue / 2;
+       
+        //Step 6: Output Results, Part A: Output using string concatenation
+        Console.WriteLine("Using Concatenation:");
+        Console.WriteLine("Sum: " + sumvalue);
+        Console.WriteLine("Average: " + averagevalue);
            
-
-
+        //Step 6: Output Results, Part B: Output using string interpolation
+        Console.WriteLine("Using Interpolation:");
+        Console.WriteLine($"Sum: {sumvalue} \n Average: {averagevalue}");
 
     }
 }
